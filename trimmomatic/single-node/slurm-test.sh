1c976579ee3c61eb4b4090fc684bddb52328d8c5
Nick Porter <nap23@unm.edu>
2025-05-16 13:28:43 -0600

    Updated reads.fq and slurm-test.sh for Trimmomatic

--- trimmomatic/single-node/slurm-test.sh
+++ trimmomatic/single-node/slurm-test.sh
@@ -8,5 +8,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=2
 
-module load trimmomatic
-/usr/bin/time -v srun -n 2 trimmomatic SE -threads 2 reads.fq trimmed.fq ILLUMINACLIP:TruSeq3-SE.fa:2:30:10
+cp /opt/spack/opt/spack/linux-sapphirerapids/trimmomatic-0.39-66mweo7ujulq25p5xz3oog6oabqt2hay/share/adapters/TruSeq3-SE.fa .
+
+module load trimmomatic time
+$(which time) -v srun -n 2 trimmomatic SE -threads 2 reads.fq trimmed.fq ILLUMINACLIP:TruSeq3-SE.fa:2:30:10
